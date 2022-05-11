80e127a795075691f6d1ab7cde93d5e91c69738a
Restu <restu@eskalink.com>
2022-05-11 12:12:46 +0700

    Tambah button change background

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,10 @@
-// get current date
-// const date = new Date('2022-05-31'),
 const date = new Date(),
 	y = date.getFullYear(),
 	m = date.getMonth();
-// get current date
 
-// get first and last date of the month
 const firstDay = new Date(y, m, 1);
 const firstDayWeek = firstDay.getDay();
 const lastDay = new Date(y, m + 1, 0);
-// get first and last date of the month
 
 const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -60,11 +55,12 @@ const generateThisMonth = () => {
 	arrayWeek.forEach((element, key) => {
 		let addClass = '';
 		let addClass2 = '';
+		addClass2 += element != '' && element < date.getDate() ? ' text-slate-700' : '';
 		let addImage = '';
 		if (element == date.getDate()) {
-			addClass = ' today group-hover:scale-[2]';
-			addClass2 = ' circle';
-			addImage = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><path d="M444.8,51.9C391.9,34,330.3,54.9,284.7,63C445.1-50.4,776,38.3,871.8,140.7c166.9,178.2,109.9,545.5-64.1,698.3c-65.4,57.4-192,141.5-298.9,149.6c-148.2,11.4-279.2-48.1-357.6-133C94,793.7,30.8,672.7,28.5,573C22.7,319.8,217.9,96.3,444.8,51.9L444.8,51.9z M615.6,76.7c52.5,28.6,141.7,47.4,153.3,100.8c-73.2-33.5-114-76.3-189-90.2C330.5,41.1,112,281.8,89.3,511.8c-13.6,137.8,60,262.8,138,329C296,899.2,418,946.3,544.1,925.7c71.4-11.6,172.8-76.8,235-132.7C991.1,602.9,980.5,83.6,615.6,76.7C609.6,60.6,615.7,67.2,615.6,76.7L615.6,76.7z"/></g></svg>';
+			addClass += ' today group-hover:scale-[2]';
+			addClass2 += ' circle';
+			addImage += '<svg class="z-0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><g><path d="M444.8,51.9C391.9,34,330.3,54.9,284.7,63C445.1-50.4,776,38.3,871.8,140.7c166.9,178.2,109.9,545.5-64.1,698.3c-65.4,57.4-192,141.5-298.9,149.6c-148.2,11.4-279.2-48.1-357.6-133C94,793.7,30.8,672.7,28.5,573C22.7,319.8,217.9,96.3,444.8,51.9L444.8,51.9z M615.6,76.7c52.5,28.6,141.7,47.4,153.3,100.8c-73.2-33.5-114-76.3-189-90.2C330.5,41.1,112,281.8,89.3,511.8c-13.6,137.8,60,262.8,138,329C296,899.2,418,946.3,544.1,925.7c71.4-11.6,172.8-76.8,235-132.7C991.1,602.9,980.5,83.6,615.6,76.7C609.6,60.6,615.7,67.2,615.6,76.7L615.6,76.7z"/></g></svg>';
 		}
 		const span = '<span class="dateSpan' + addClass + '">' + element + '</span>';
 		const dateView = '<td class="group' + addClass2 + '">' + addImage + span + '</td>';
@@ -111,13 +107,13 @@ window.onload = (event) => {
 	const todoSection = document.querySelector('#todo-card');
 	todoSection.classList.remove('opacity-0');
 	todoSection.classList.add('opacity-100');
-	
+
 	setTimeout(() => {
 		const monthCalSection = document.querySelector('#monthly-view');
 		monthCalSection.classList.remove('opacity-0');
 		monthCalSection.classList.add('opacity-100');
 	}, 1000);
-	
+
 	setTimeout(() => {
 		const quoteSection = document.querySelector('#quote-view');
 		quoteSection.classList.remove('opacity-0');
@@ -126,3 +122,57 @@ window.onload = (event) => {
 	}, 2000);
 	console.log('page is fully loaded');
 };
+
+let currentBg = 'bg-splash1';
+let currentBg2 = 'bg-splash2';
+let activeBg = 2;
+let bgSource = '';
+let fileName = '';
+
+const refreshBg = () => {
+	console.log('clicked refresh');
+	const bgImage = document.querySelector('#bg-image1');
+	const bgImage2 = document.querySelector('#bg-image2');
+
+	if (activeBg == 2) {
+		bgImage.classList.remove('opacity-0');
+		bgImage.classList.add('opacity-100');
+
+		bgImage2.classList.add('opacity-0');
+		bgImage2.classList.remove('opacity-100');
+	} else {
+		bgImage2.classList.remove('opacity-0');
+		bgImage2.classList.add('opacity-100');
+
+		bgImage.classList.add('opacity-0');
+		bgImage.classList.remove('opacity-100');
+	}
+
+	let newFileName = Math.floor(Math.random() * 17) + 1;
+	while (newFileName == fileName) {
+		newFileName = Math.floor(Math.random() * 17) + 1;
+	}
+	fileName = newFileName;
+
+	setTimeout(() => {
+		bgSource = 'bg-splash' + fileName;
+
+		if (currentBg != '' && activeBg == 1) {
+			bgImage.classList.remove(currentBg);
+			bgImage.classList.add(bgSource);
+			currentBg = bgSource;
+		}
+
+		if (currentBg2 != '' && activeBg == 2) {
+			bgImage2.classList.remove(currentBg2);
+			bgImage2.classList.add(bgSource);
+			currentBg2 = bgSource;
+		}
+
+		activeBg = activeBg == 1 ? 2 : 1;
+		console.log(activeBg);
+	}, 1100);
+};
+
+const bgChange = document.querySelector('#bg-change');
+bgChange.addEventListener('click', refreshBg);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 module.exports = {
-	content: ['index.html', 'script.js'],
+	content: ['index.html', 'dist/js/script.js'],
 	theme: {
 		extend: {
 			backgroundImage: {
 				// 'back-splash': "url('https://source.unsplash.com/1280x720?nature')",
 				'back-splash': "url('https://source.unsplash.com/854x480?nature')",
+				'splash1': "url('../../dist/img/background/1.jpg')",
+				'splash2': "url('../../dist/img/background/2.jpg')",
+				'splash3': "url('../../dist/img/background/3.jpg')",
+				'splash4': "url('../../dist/img/background/4.jpg')",
+				'splash5': "url('../../dist/img/background/5.jpg')",
+				'splash6': "url('../../dist/img/background/6.jpg')",
+				'splash7': "url('../../dist/img/background/7.jpg')",
+				'splash8': "url('../../dist/img/background/8.jpg')",
+				'splash9': "url('../../dist/img/background/9.jpg')",
+				'splash10': "url('../../dist/img/background/10.jpg')",
+				'splash11': "url('../../dist/img/background/11.jpg')",
+				'splash12': "url('../../dist/img/background/12.jpg')",
+				'splash13': "url('../../dist/img/background/13.jpg')",
+				'splash14': "url('../../dist/img/background/14.jpg')",
+				'splash15': "url('../../dist/img/background/15.jpg')",
+				'splash16': "url('../../dist/img/background/16.jpg')",
+				'splash17': "url('../../dist/img/background/17.jpg')",
 			},
 			backgroundSize: {
 				'custom': '4rem',
